16481b3751b5e06a725542efdc1b730ce0449dbc
Alexandr Olferuk <adamant94@mail.ru>
2014-03-20 11:28:50 +0400

    Added Memento

--- SpacePewPew/Game.cs
+++ SpacePewPew/Game.cs
@@ -3,7 +3,22 @@ using SpacePewPew.Ships;
 
 namespace SpacePewPew
 {
-    class Game
+    public class GameState
+    {
+         
+    }
+
+    public class Memento
+    {
+        public Memento(GameState gameState)
+        {
+            State = gameState;
+        }
+
+        public GameState State { get; private set; }
+    }
+
+    public class Game
     {
         #region Singleton pattern
         private static Game _instance;
@@ -14,12 +29,35 @@ namespace SpacePewPew
 
         public static Game Instance()
         {
-            // отложенная инициализация. Ништячки.
             return _instance ?? (_instance = new Game());
         }
         #endregion
 
+        #region Declarations
         private Map _map;
         private List<Ship> _ships;
+        #endregion
+        
+        #region
+        private GameState GameState { get; set; }
+
+        public Memento CreateMemento()
+        {
+            return new Memento(GameState);
+        }
+
+        public void LoadState(Memento memento)
+        {
+            GameState = memento.State;
+        }
+       
+        private List<Memento> memento;
+
+        public void SaveState()
+        {
+            memento.Add(CreateMemento());
+        }
+        #endregion
+
     }
 }
