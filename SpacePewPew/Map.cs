33441dda01d315be7146e7a01e8e1b5f1395f6a6
Dmitriy Mubarakshin <dimonmub@mail.ru>
2014-03-20 22:54:23 +0400

    Dimon's first commit

--- SpacePewPew/Map.cs
+++ SpacePewPew/Map.cs
@@ -3,5 +3,11 @@
     // TODO: [Мубаракшин Диман] работай с этим классом.
     public class Map
     {
+
+    }
+    public struct Cell
+    {
+        private double size;
+        private byte state;
     }
 }
